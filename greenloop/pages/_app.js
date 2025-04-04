30ab69cdf0b20ff2f43340b57876b466a4306c15
Wiz Tech <96401649+Wiz-Tech-Kid@users.noreply.github.com>
2025-04-04 14:34:22 +0200

    Add greenloop/pages/_app.js 1743770062861

--- greenloop/pages/_app.js
+++ greenloop/pages/_app.js
@@ -1,6 +1,37 @@
 import '../styles/globals.css'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as Sentry from '@sentry/nextjs'
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    // Initialize error tracking
+    if (process.env.NEXT_PUBLIC_SENTRY_DSN) {
+      Sentry.init({
+        dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+        environment: process.env.NODE_ENV,
+        tracesSampleRate: 1.0
+      })
+    }
+
+    // Handle route changes
+    const handleRouteChange = (url) => {
+      // Track page views
+      if (window.gtag) {
+        window.gtag('config', process.env.NEXT_PUBLIC_GA_ID, {
+          page_path: url,
+        })
+      }
+    }
+
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [])
+
   return <Component {...pageProps} />
 }
 
